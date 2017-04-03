bdc690b0bb55563b6813776a2baf00210d556382
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-04-03 12:13:15 -0400

    star system not persisting data

--- public/js/script.js
+++ public/js/script.js
@@ -2,24 +2,23 @@ $(document).ready(function(){
 
 $("#star-1").click(function(){
   $(".star-bar").css("width", "20%");
-  // $("#star-val").val(1);
-  console.log("dfd");
+  $("#star-val").val(1);
 })
 $("#star-2").click(function(){
   $(".star-bar").css("width", "40%");
-  // $("#star-val").val(2);
+  $("#star-val").val(2);
 })
 $("#star-3").click(function(){
   $(".star-bar").css("width", "60%");
-  // $("#star-val").val(3);
+  $("#star-val").val(3);
 })
 $("#star-4").click(function(){
   $(".star-bar").css("width", "80%");
-  // $("#star-val").val(4);
+  $("#star-val").val(4);
 })
 $("#star-5").click(function(){
   $(".star-bar").css("width", "100%");
-  // $("#star-val").val(5);
+  $("#star-val").val(5);
 })
 
 })

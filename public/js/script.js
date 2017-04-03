da99322db8621f002ed958c7df6686f256482d9d
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-04-03 15:51:50 -0400

    added randomized quotes box

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,21 @@
-$(document).ready(function(){
+$(document).ready(() => {
+let quotes = [
+  "It's not what you look at that matters, it's what you see.",
+  "The price of anything is the amount of life you exchange for it.",
+  "You must live in the present, launch yourself on every wave, find your eternity in each moment.",
+  "Go confidently in the direction of your dreams. Live the life you have imagined.",
+  "If you have built castles in the air, your work need not be lost; that is where they should be. Now put the foundations under them.",
+  "It's not what you look at that matters, it's what you see.",
+  "Aim above morality. Be not simply good, be good for something.",
+  "Disobedience is the true foundation of liberty. The obedient must be slaves.",
+  "The universe is wider than our views of it.",
+  "Truths and roses have thorns about them."
+]
 
-$("#star-1").click(function(){
-  $(".star-bar").css("width", "20%");
-  $("#star-val").val(1);
-})
-$("#star-2").click(function(){
-  $(".star-bar").css("width", "40%");
-  $("#star-val").val(2);
-})
-$("#star-3").click(function(){
-  $(".star-bar").css("width", "60%");
-  $("#star-val").val(3);
-})
-$("#star-4").click(function(){
-  $(".star-bar").css("width", "80%");
-  $("#star-val").val(4);
-})
-$("#star-5").click(function(){
-  $(".star-bar").css("width", "100%");
-  $("#star-val").val(5);
-})
+function randomize(quotes){
+  let random = quotes[Math.floor(Math.random()*quotes.length)]
+  $('#quotes-box').html(random)
+}
 
+randomize(quotes)
 })
